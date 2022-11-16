b1fd12a2a9752176e6cf3f2312ca96c1e8778b3d
Amaz <worachanon226@hotmail.com>
2022-11-16 12:56:06 +0700

    Update getpods func

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"context"
 	"flag"
-	"net/http"
 	"path/filepath"
-	"strconv"
 
-	"github.com/gin-gonic/gin"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
-func getpods(c *gin.Context) {
+func getpods(name string, kubeconfig *string) {
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods(name).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return len(pods.Items)
+}
+
+func main(){
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -22,29 +38,6 @@ func getpods(c *gin.Context) {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
-
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	c.String(http.StatusOK, strconv.Itoa((len(pods.Items))))
-}
-
-func main() {
-
-	r := gin.Default()
-	r.GET("/getpods", getpods)
-	r.Run()
-
+	pods := getpods("default",kubeconfig)
+	fmt.Println(pods)
 }
