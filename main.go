65ae330cc32e89ab8c2635e169f60053f360deb9
Amaz <worachanon226@hotmail.com>
2022-11-17 16:54:38 +0700

    Update updateDeployment

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func createDeployment(clientset *kubernetes.Clientset) {
 	prompt()
 }
 
-func updateDeployment(clientset *kubernetes.Clientset) {
+func updateDeployment(setReplicas int32, setContainer string, clientset *kubernetes.Clientset) {
 	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
 
 	fmt.Println("Updating deployment...")
@@ -83,16 +83,16 @@ func updateDeployment(clientset *kubernetes.Clientset) {
 	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		result, getErr := deploymentsClient.Get(context.TODO(), "demo-deployment", metav1.GetOptions{})
 		if getErr != nil {
-			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
+			panic(fmt.Errorf("failed to get latest version of Deployment: %v", getErr))
 		}
 
-		result.Spec.Replicas = int32Ptr(1)                           // reduce replica count
-		result.Spec.Template.Spec.Containers[0].Image = "nginx:1.13" // change nginx version
+		result.Spec.Replicas = int32Ptr(setReplicas)
+		result.Spec.Template.Spec.Containers[0].Image = setContainer
 		_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
 		return updateErr
 	})
 	if retryErr != nil {
-		panic(fmt.Errorf("Update failed: %v", retryErr))
+		panic(fmt.Errorf("update failed: %v", retryErr))
 	}
 	fmt.Println("Updated deployment...")
 	prompt()
@@ -135,7 +135,7 @@ func main() {
 	fmt.Println(pods)
 
 	createDeployment(clientset)
-	updateDeployment(clientset)
+	updateDeployment(1, "nginx:1.13", clientset)
 	deleteDeployment("demo-deployment", clientset)
 }
 
