cbab235eaf984e511b19eadc1e6ad78570b4aa8c
Amaz <worachanon226@hotmail.com>
2022-11-17 16:50:57 +0700

    Update updateDeployment

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/util/retry"
 )
 
 func getpods(name string, clientset *kubernetes.Clientset) int {
@@ -74,6 +75,29 @@ func createDeployment(clientset *kubernetes.Clientset) {
 	prompt()
 }
 
+func updateDeployment(clientset *kubernetes.Clientset) {
+	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+
+	fmt.Println("Updating deployment...")
+
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		result, getErr := deploymentsClient.Get(context.TODO(), "demo-deployment", metav1.GetOptions{})
+		if getErr != nil {
+			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
+		}
+
+		result.Spec.Replicas = int32Ptr(1)                           // reduce replica count
+		result.Spec.Template.Spec.Containers[0].Image = "nginx:1.13" // change nginx version
+		_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
+		return updateErr
+	})
+	if retryErr != nil {
+		panic(fmt.Errorf("Update failed: %v", retryErr))
+	}
+	fmt.Println("Updated deployment...")
+
+}
+
 func deleteDeployment(name string, clientset *kubernetes.Clientset) {
 	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
 
@@ -111,6 +135,7 @@ func main() {
 	fmt.Println(pods)
 
 	createDeployment(clientset)
+	updateDeployment(clientset)
 	deleteDeployment("demo-deployment", clientset)
 }
 
